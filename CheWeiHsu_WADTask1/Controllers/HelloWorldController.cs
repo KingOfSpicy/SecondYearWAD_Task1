3348811276ab7e8c59233072dd5f003725041a26
KingOfSpicy <sangowosan@gmail.com>
2022-02-26 13:06:19 +0200

    Add the controller.

---
+++ CheWeiHsu_WADTask1/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheWeiHsu_WADTask1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+       
+            // 
+            // GET: /HelloWorld/
+
+            public string Index()
+            {
+                return "This is my default action...";
+            }
+
+            // 
+            // GET: /HelloWorld/Welcome/ 
+
+            public string Welcome()
+            {
+                return "This is the Welcome action method...";
+            }
+        
+    }
+}
